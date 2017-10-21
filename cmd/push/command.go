81ec7a400bcc4ed677a78498cbe0de6601683900
Ace-Tang <aceapril@126.com>
2017-10-22 06:27:23 +0800

    add push command

--- cmd/push/command.go
+++ cmd/push/command.go
@@ -2,8 +2,15 @@ package push
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	defaultRegistry = "127.0.0.1:5000"
+	defaultDir      = ".ketos/tags"
+	defaultlLayer   = ".ketos/layers"
 )
 
 var (
@@ -20,9 +27,30 @@ func pushMain(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
+	name, tag, err := parseRef(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	err = push(name, tag)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("image %s push successful\n", args[0])
+
 	return nil
 }
 
-func parseRef(ref string) {
+func parseRef(ref string) (string, string, error) {
+	repo := strings.Split(ref, ":")
+	if len(repo) > 2 {
+		return "", "", fmt.Errorf("%s format error", ref)
+	}
 
+	if len(repo) == 2 {
+		return repo[0], repo[1], nil
+	}
+
+	return repo[0], "latest", nil
 }
--- cmd/push/push.go
+++ cmd/push/push.go
@@ -1,5 +1,90 @@
 package push
 
-func push() error {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	manifestV2 "github.com/docker/distribution/manifest/schema2"
+	"github.com/setekhid/ketos/client"
+)
+
+func push(name, tag string) error {
+	hub, err := client.NewRegitry("127.0.0.1", "", "")
+	if err != nil {
+		return err
+	}
+
+	manifest, err := marshalManifest(defaultDir)
+	if err != nil {
+		return err
+	}
+
+	for _, l := range manifest.Layers {
+		digest := l.Digest
+		exist, err := hub.HasLayer(name, digest)
+		if err != nil {
+			return err
+		}
+		if exist {
+			continue
+		}
+
+		layer := l.Digest.Encoded()
+		hasL := getLayer(layer)
+		if !hasL {
+			fmt.Printf("layer %s not exist in %s\n", layer, defaultlLayer)
+			return nil
+		}
+
+		gz := filepath.Join(defaultlLayer, layer+".tar.gz")
+		contents, err := os.Open(gz)
+		if err != nil {
+			return err
+		}
+
+		err = hub.UploadLayer(name, digest, contents)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("layer %s push successful\n", layer)
+	}
+
 	return nil
+}
+
+func getLayer(layer string) bool {
+	subdir, err := ioutil.ReadDir(defaultlLayer)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	for _, d := range subdir {
+		if d.Name() == layer {
+			return true
+		}
+	}
+
+	return false
+}
+
+func marshalManifest(baseDir string) (*manifestV2.DeserializedManifest, error) {
+	path := filepath.Join(baseDir, "manifest.json")
+	fm, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	manifest := &manifestV2.DeserializedManifest{}
+
+	err = json.NewDecoder(fm).Decode(manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	return manifest, nil
 }
